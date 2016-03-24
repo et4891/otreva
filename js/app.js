c29ff0c481b29501d982795d7693d6e661442362
ET-Asus <et1103@hotmail.com>
2016-03-24 06:31:01 -0700

    with help online, select / deselect the features work. Invest the codes and see if I can come up with my own solution. Changed index.html, app.js, style.css

--- js/app.js
+++ js/app.js
@@ -42,23 +42,23 @@
     });
 
     //Controller for app features
-    app.controller("FeaturesController", function(){
+    app.controller("FeaturesController", function($scope){
        this.apps = features;
        this.listAppPrices = [];
 
-       // add name and price into the new array which is used to show in the table
-       this.addPrices = function(name, price){
-           //Checks if the exact name, price property exists in the array and return boolean
-           var found = this.listAppPrices.some(function (e){
-               console.log(e.name);
-              return ((e.name === name) && (e.price === price)) ;
-           });
+       // // add name and price into the new array which is used to show in the table
+       // this.addPrices = function(name, price){
+       //     //Checks if the exact name, price property exists in the array and return boolean
+       //     var found = this.listAppPrices.some(function (e){
+       //         console.log(e.name);
+       //        return ((e.name === name) && (e.price === price)) ;
+       //     });
 
-           //If found not true then push the new values into the array
-           if(!found){
-               this.listAppPrices.push({name: name, price: price});
-           }
-       };
+       //     //If found not true then push the new values into the array
+       //     if(!found){
+       //         this.listAppPrices.push({name: name, price: price});
+       //     }
+       // };
 
        // adds all the prices of the array which gives the total
        this.totalAppPrice = function(){
@@ -69,9 +69,61 @@
            return total;
        };
 
-       // remove the whole object in the array when remove is clicked
-       this.remove = function(index) {
-           this.listAppPrices.splice(index, 1);
-       };
+       // // remove the whole object in the array when remove is clicked
+       // this.remove = function(index) {
+       //     this.listAppPrices.splice(index, 1);
+       // };
+       $scope.dummyArray = [];
+        var f = 0,
+            x = 0,
+            rem = false;
+        this.setSelected = function(app, index) {
+            console.log("app ", app);
+
+            //remove an item
+            if (app.selected) {
+                console.log(" list ", $scope.dummyArray);
+                $scope.dummyArray.forEach(function(e, ind) {
+                    if (e.name === app.name) {
+                        console.log(ind, " e ", e);
+                        rem = true;
+                        $scope.dummyArray.splice(ind, 1);
+                    }
+                });
+                console.log("dumm ", $scope.dummyArray);
+
+                this.listAppPrices = $scope.dummyArray;
+
+            } else {
+                rem = false;
+            }
+            //used to select a div and change its colour
+            app.selected ? app.selected = false : app.selected = true;
+
+            //add an item
+            if (!rem) {
+                if ($scope.dummyArray.length) {
+                    $scope.dummyArray.forEach(function(each) {
+                        console.log("each ");
+                        if (each.name !== app.name) {
+                            console.log("inside if ");
+                            f = 1;
+                        }
+                    });
+                } else {
+                    console.log("inside else ");
+                    $scope.dummyArray.push(app);
+
+                }
+                if (f === 1) {
+                    f = 0;
+                    console.log("push");
+                    $scope.dummyArray.push(app);
+                }
+                console.log(" list--> ", $scope.dummyArray.length);
+                this.listAppPrices = $scope.dummyArray;
+            }
+
+        }
     });
 })();
